edba4ec7543469c8a35fbaf6e7b7148747516733
Dmitriy <whittemak@gmail.com>
2022-02-06 16:32:57 +0300

    доп задачи

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,28 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        for (int i = 1; i <= 30; i++) {
+            System.out.println(i + ": ");
+            if (i % 3 == 0) {
+                System.out.println(i + " :ping");
+            }
+            if (i % 5 == 0) {
+                System.out.println(i + " :pong");
+            }
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i + ":ping pong ");
+            }
+            System.out.println();
+            int a = 0;
+            int b = 1;
+            for (int y = 0; y <= 4; y++) {
+                System.out.print(a + " " + b + " ");
+                a = a + b;
+                b = b + a;
+            }
+        }
     }
 }
+
+
+
